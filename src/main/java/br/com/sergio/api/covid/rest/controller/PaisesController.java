671d6213e9893cbc9c2e90a036df4f144359562e
Sérgio Barbosa <sergio.natan@oobj.com.br>
2024-03-19 22:17:51 -0300

    Retirando dependencias desnecessárias

--- src/main/java/br/com/sergio/api/covid/rest/controller/PaisesController.java
+++ src/main/java/br/com/sergio/api/covid/rest/controller/PaisesController.java
@@ -5,8 +5,6 @@ import static br.com.sergio.api.covid.utils.Constantes.CASOS;
 import static br.com.sergio.api.covid.utils.Constantes.MORTES;
 
 import br.com.sergio.api.covid.rest.service.PaisesService;
-import br.com.sergio.api.covid.utils.ConstrutorDeURL;
-import br.com.sergio.api.covid.utils.ConversorDados;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
