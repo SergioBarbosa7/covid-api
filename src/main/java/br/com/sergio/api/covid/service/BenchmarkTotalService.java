bee553cfc47f262d0c034ab36d773a4311162980
Sérgio Barbosa <sergio.natan@oobj.com.br>
2024-03-21 15:06:15 -0300

    Criando serviço para coordenar as operações dos Benchmarks Totais

---
+++ src/main/java/br/com/sergio/api/covid/service/BenchmarkTotalService.java
@@ -0,0 +1,45 @@
+package br.com.sergio.api.covid.service;
+
+import br.com.sergio.api.covid.model.BenchmarkPais;
+import br.com.sergio.api.covid.model.BenchmarkTotal;
+import br.com.sergio.api.covid.processador.ProcessadorBenchmarkTotal;
+import br.com.sergio.api.covid.repository.BenchmarkTotalRepository;
+import br.com.sergio.api.covid.utils.ConversorDados;
+import org.springframework.stereotype.Service;
+import java.util.Optional;
+
+@Service
+public class BenchmarkTotalService {
+	
+	BenchmarkPaisService benchmarkPaisService;
+	ProcessadorBenchmarkTotal processadorBenchmarkTotal;
+	ConversorDados conversorDados;
+	BenchmarkTotalRepository benchmarkTotalRepository;
+	
+	public BenchmarkTotalService(BenchmarkPaisService benchmarkPaisService,
+								 ProcessadorBenchmarkTotal processadorBenchmarkTotal,
+								 BenchmarkTotalRepository benchmarkTotalRepository) {
+		this.benchmarkPaisService = benchmarkPaisService;
+		this.processadorBenchmarkTotal = processadorBenchmarkTotal;
+		this.benchmarkTotalRepository = benchmarkTotalRepository;
+	}
+	
+	public BenchmarkTotal criaBenchmarkTotal(String nome, String pais1, String pais2, String dataInicial,
+											 String dataFinal) {
+		BenchmarkPais benchmarkPais1 = benchmarkPaisService.obtemBenchmarkPais(pais1, dataInicial, dataFinal);
+		BenchmarkPais benchmarkPais2 = benchmarkPaisService.obtemBenchmarkPais(pais2, dataInicial, dataFinal);
+		BenchmarkTotal benchmarkTotal = processadorBenchmarkTotal.processaBenchmarkTotal(nome, benchmarkPais1,
+				benchmarkPais2);
+		return benchmarkTotalRepository.saveAndFlush(benchmarkTotal);
+	}
+	
+	public BenchmarkTotal obtemOptionalBenchmarkTotalPeloId(Long id){
+		Optional<BenchmarkTotal> optionalBenchmark =  benchmarkTotalRepository.findById(id);
+		if(optionalBenchmark.isPresent()){
+			return optionalBenchmark.get();
+		}
+		
+		return null;
+	}
+	
+}
