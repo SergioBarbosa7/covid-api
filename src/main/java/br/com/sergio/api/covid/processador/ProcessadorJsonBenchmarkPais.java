290b34a48d0be830742b883e072e602a3b6de2a4
Sérgio Barbosa <sergio.natan@oobj.com.br>
2024-03-21 15:00:25 -0300

    Criando processador para os dados do json que serão armazenados

---
+++ src/main/java/br/com/sergio/api/covid/processador/ProcessadorJsonBenchmarkPais.java
@@ -0,0 +1,25 @@
+package br.com.sergio.api.covid.processador;
+
+import br.com.sergio.api.covid.model.DadosPorDia;
+import br.com.sergio.api.covid.model.JsonsBenchmarkPais;
+import br.com.sergio.api.covid.utils.ConversorDados;
+import com.google.gson.reflect.TypeToken;
+import org.springframework.stereotype.Component;
+import java.util.List;
+
+@Component
+public class ProcessadorJsonBenchmarkPais {
+	private ConversorDados conversorDados;
+	
+	public ProcessadorJsonBenchmarkPais(ConversorDados conversorDados) {
+		this.conversorDados = conversorDados;
+	}
+	
+	public JsonsBenchmarkPais converteDadosDiaParaJson(List<DadosPorDia> casosPorDia, List<DadosPorDia> mortesPorDia){
+		JsonsBenchmarkPais jsonsBenchmarkPais = new JsonsBenchmarkPais();
+		jsonsBenchmarkPais.setJsonCasos(conversorDados.converterDadosParaJson(casosPorDia, new TypeToken<List<DadosPorDia>>(){}.getType()));
+		jsonsBenchmarkPais.setJsonMortes(conversorDados.converterDadosParaJson(mortesPorDia, new TypeToken<List<DadosPorDia>>(){}.getType()));
+		return jsonsBenchmarkPais;
+	}
+	
+}
