7fbfa1958957b3b4436a6616a67e388f9edb9a09
Sérgio Barbosa <sergio.natan@oobj.com.br>
2024-03-21 09:55:41 -0300

    Criado um pacote para armazenar os dados de mortes e casos, para posteriormente ser convertida em uma benchmark de um país

---
+++ src/main/java/br/com/sergio/api/covid/external/PacoteCasosEMortes.java
@@ -0,0 +1,54 @@
+package br.com.sergio.api.covid.external;
+
+import br.com.sergio.api.covid.model.DadosPorDia;
+import java.time.LocalDate;
+import java.util.Map;
+
+public class PacoteCasosEMortes {
+	private String pais;
+	private LocalDate dataInicio;
+	private LocalDate dataFinal;
+	private Map<String, ExternalDadosPorDia> dadosMortes;
+	private Map<String, ExternalDadosPorDia> dadosCasos;
+	
+	public String getPais() {
+		return pais;
+	}
+	
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+	
+	public Map<String, ExternalDadosPorDia> getDadosMortes() {
+		return dadosMortes;
+	}
+	
+	public void setDadosMortes(Map<String, ExternalDadosPorDia> dadosMortes) {
+		this.dadosMortes = dadosMortes;
+	}
+	
+	public Map<String, ExternalDadosPorDia> getDadosCasos() {
+		return dadosCasos;
+	}
+	
+	public void setDadosCasos(Map<String, ExternalDadosPorDia> dadosCasos) {
+		this.dadosCasos = dadosCasos;
+	}
+	
+	public LocalDate getDataInicio() {
+		return dataInicio;
+	}
+	
+	public void setDataInicio(LocalDate dataInicio) {
+		this.dataInicio = dataInicio;
+	}
+	
+	public LocalDate getDataFinal() {
+		return dataFinal;
+	}
+	
+	public void setDataFinal(LocalDate dataFinal) {
+		this.dataFinal = dataFinal;
+	}
+	
+}
