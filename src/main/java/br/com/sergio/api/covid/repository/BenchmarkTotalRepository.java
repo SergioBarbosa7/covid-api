4b5f6ccbc035ab8978b01cdc1fcba5df157f82af
Sérgio Barbosa <sergio.natan@oobj.com.br>
2024-03-21 15:02:48 -0300

    Adicionando anotação de repository

--- src/main/java/br/com/sergio/api/covid/repository/BenchmarkTotalRepository.java
+++ src/main/java/br/com/sergio/api/covid/repository/BenchmarkTotalRepository.java
@@ -2,7 +2,9 @@ package br.com.sergio.api.covid.repository;
 
 import br.com.sergio.api.covid.model.BenchmarkTotal;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BenchmarkTotalRepository extends JpaRepository<BenchmarkTotal, Long> {
 
 }
