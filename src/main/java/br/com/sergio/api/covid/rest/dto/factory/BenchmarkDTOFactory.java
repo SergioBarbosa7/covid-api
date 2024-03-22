c5ecac0ac84e8cd4fbb6e8e3779519673290e768
Sérgio Barbosa <sergio.natan@oobj.com.br>
2024-03-22 10:11:19 -0300

    Iniciando criação de Factory para os DTOs

---
+++ src/main/java/br/com/sergio/api/covid/rest/dto/factory/BenchmarkDTOFactory.java
@@ -0,0 +1,17 @@
+package br.com.sergio.api.covid.rest.dto.factory;
+
+import br.com.sergio.api.covid.model.BenchmarkPais;
+import br.com.sergio.api.covid.model.BenchmarkTotal;
+import br.com.sergio.api.covid.rest.dto.BenchmarkPaisDTO;
+import br.com.sergio.api.covid.rest.dto.BenchmarkTotalDTO;
+
+public class BenchmarkDTOFactory {
+
+	public BenchmarkTotalDTO geraDTOBenchmark(BenchmarkTotal benchmarkTotal){
+	
+	}
+	
+	public BenchmarkPaisDTO geraDTOBenchmarkPais(BenchmarkPais benchmarkPais){
+	
+	}
+}
