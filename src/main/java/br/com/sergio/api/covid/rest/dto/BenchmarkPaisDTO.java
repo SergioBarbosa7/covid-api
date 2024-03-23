eada1fcf1bd498abe31826e2951c51399b64891d
Sérgio Barbosa <sergio.natan@oobj.com.br>
2024-03-23 09:13:45 -0300

    Criando métodos para preenchimento das estatisticas no DTO

--- src/main/java/br/com/sergio/api/covid/rest/dto/BenchmarkPaisDTO.java
+++ src/main/java/br/com/sergio/api/covid/rest/dto/BenchmarkPaisDTO.java
@@ -1,5 +1,143 @@
 package br.com.sergio.api.covid.rest.dto;
 
-public class BenchmarkPaisDTO {
+import java.util.Date;
 
+public class BenchmarkPaisDTO {
+	
+	private String nomePais;
+	private Integer diasNoPeriodo;
+	private Double taxaDeMortalidade;
+	private Date dataInicial;
+	private Date dataFinal;
+	private Integer casosIniciais;
+	private Integer mortesIniciais;
+	private Integer mortesNoPeriodo;
+	private Integer casosNoPeriodo;
+	private Integer picoDeMortes;
+	private Integer picoDeCasos;
+	private Date dataPicoDeCasos;
+	private Date dataPicoDeMortes;
+	private Integer mediaDeCasosPorDia;
+	private Integer mediaDeMortesPorDia;
+	
+	public String getNomePais() {
+		return nomePais;
+	}
+	
+	public void setNomePais(String nomePais) {
+		this.nomePais = nomePais;
+	}
+	
+	public Double getTaxaDeMortalidade() {
+		return taxaDeMortalidade;
+	}
+	
+	public void setTaxaDeMortalidade(Double taxaDeMortalidade) {
+		this.taxaDeMortalidade = taxaDeMortalidade;
+	}
+	
+	public Date getDataInicial() {
+		return dataInicial;
+	}
+	
+	public void setDataInicial(Date dataInicial) {
+		this.dataInicial = dataInicial;
+	}
+	
+	public Date getDataFinal() {
+		return dataFinal;
+	}
+	
+	public void setDataFinal(Date dataFinal) {
+		this.dataFinal = dataFinal;
+	}
+	
+	public Integer getCasosIniciais() {
+		return casosIniciais;
+	}
+	
+	public void setCasosIniciais(Integer casosIniciais) {
+		this.casosIniciais = casosIniciais;
+	}
+	
+	public Integer getMortesIniciais() {
+		return mortesIniciais;
+	}
+	
+	public void setMortesIniciais(Integer mortesIniciais) {
+		this.mortesIniciais = mortesIniciais;
+	}
+	
+	public Integer getMortesNoPeriodo() {
+		return mortesNoPeriodo;
+	}
+	
+	public void setMortesNoPeriodo(Integer mortesNoPeriodo) {
+		this.mortesNoPeriodo = mortesNoPeriodo;
+	}
+	
+	public Integer getCasosNoPeriodo() {
+		return casosNoPeriodo;
+	}
+	
+	public void setCasosNoPeriodo(Integer casosNoPeriodo) {
+		this.casosNoPeriodo = casosNoPeriodo;
+	}
+	
+	public Integer getPicoDeMortes() {
+		return picoDeMortes;
+	}
+	
+	public void setPicoDeMortes(Integer picoDeMortes) {
+		this.picoDeMortes = picoDeMortes;
+	}
+	
+	public Integer getPicoDeCasos() {
+		return picoDeCasos;
+	}
+	
+	public void setPicoDeCasos(Integer picoDeCasos) {
+		this.picoDeCasos = picoDeCasos;
+	}
+	
+	public Date getDataPicoDeCasos() {
+		return dataPicoDeCasos;
+	}
+	
+	public void setDataPicoDeCasos(Date dataPicoDeCasos) {
+		this.dataPicoDeCasos = dataPicoDeCasos;
+	}
+	
+	public Date getDataPicoDeMortes() {
+		return dataPicoDeMortes;
+	}
+	
+	public void setDataPicoDeMortes(Date dataPicoDeMortes) {
+		this.dataPicoDeMortes = dataPicoDeMortes;
+	}
+	
+	public Integer getMediaDeCasosPorDia() {
+		return mediaDeCasosPorDia;
+	}
+	
+	public void setMediaDeCasosPorDia(Integer mediaDeCasosPorDia) {
+		this.mediaDeCasosPorDia = mediaDeCasosPorDia;
+	}
+	
+	public Integer getMediaDeMortesPorDia() {
+		return mediaDeMortesPorDia;
+	}
+	
+	public void setMediaDeMortesPorDia(Integer mediaDeMortesPorDia) {
+		this.mediaDeMortesPorDia = mediaDeMortesPorDia;
+	}
+	
+	public Integer getDiasNoPeriodo() {
+		return diasNoPeriodo;
+	}
+	
+	public void setDiasNoPeriodo(Integer diasNoPeriodo) {
+		this.diasNoPeriodo = diasNoPeriodo;
+	}
+	
 }
--- src/main/java/br/com/sergio/api/covid/rest/dto/factory/BenchmarkDTOFactory.java
+++ src/main/java/br/com/sergio/api/covid/rest/dto/factory/BenchmarkDTOFactory.java
@@ -11,7 +11,46 @@ public class BenchmarkDTOFactory {
 	
 	}
 	
-	public BenchmarkPaisDTO geraDTOBenchmarkPais(BenchmarkPais benchmarkPais){
+	public BenchmarkPaisDTO geraDTOBenchmarkPais(BenchmarkPais benchmarkPais) {
+		BenchmarkPaisDTO dto = new BenchmarkPaisDTO();
+		
+		dto.setNomePais(benchmarkPais.getNomePais());
+		dto.setCasosNoPeriodo(benchmarkPais.getCasosNoPeriodo());
+		dto.setMortesNoPeriodo(benchmarkPais.getMortesNoPeriodo());
+		dto.setDataInicial(benchmarkPais.getDataInicial());
+		dto.setDataFinal(benchmarkPais.getDataFinal());
+		preencheDiferencaDeDatas(dto);
+		preencherEstatisticasPais(dto, benchmarkPais);
+		return dto;
+	}
 	
+	public void preencherEstatisticasPais(BenchmarkPaisDTO dto, BenchmarkPais benchmarkPais) {
+		Double mortalidade = (double) (benchmarkPais.getMortesNoPeriodo() / benchmarkPais.getCasosNoPeriodo()) * 100;
+		dto.setTaxaDeMortalidade(mortalidade);
+		preencherEstatisticasMortePais(dto, processadorDadosDia.obtemDadosPorDiaDoJson(
+				benchmarkPais.getJsonsBenchmarkPais().getJsonMortes()));
+		preencherEstatisticasCasosPais(dto, processadorDadosDia.obtemDadosPorDiaDoJson(
+				benchmarkPais.getJsonsBenchmarkPais().getJsonCasos()));
+	}
+	
+	public void preencherEstatisticasMortePais(BenchmarkPaisDTO dto, List<DadosPorDia> dadosMortes) {
+		dto.setMortesIniciais(processadorDadosDia.obtemTotalInicial(dadosMortes));
+		DadosPorDia dadosPico = processadorDadosDia.obtemPicoPorDia(dadosMortes);
+		dto.setPicoDeMortes(dadosPico.getNovos());
+		dto.setDataPicoDeMortes(Date.valueOf(dadosPico.getData()));
+		dto.setMediaDeMortesPorDia(dto.getMortesNoPeriodo()/ dto.getDiasNoPeriodo());
+	}
+	
+	public void preencherEstatisticasCasosPais(BenchmarkPaisDTO dto, List<DadosPorDia> dadosCasos) {
+		dto.setCasosIniciais(processadorDadosDia.obtemTotalInicial(dadosCasos));
+		DadosPorDia dadosPico = processadorDadosDia.obtemPicoPorDia(dadosCasos);
+		dto.setPicoDeCasos(dadosPico.getNovos());
+		dto.setDataPicoDeCasos(Date.valueOf(dadosPico.getData()));
+		dto.setMediaDeCasosPorDia(dto.getCasosNoPeriodo()/ dto.getDiasNoPeriodo());
+	}
+	
+	public void preencheDiferencaDeDatas(BenchmarkPaisDTO dto){
+		long diferenca = dto.getDataFinal().getTime() - dto.getDataInicial().getTime();
+		dto.setDiasNoPeriodo((int) TimeUnit.DAYS.convert(diferenca, TimeUnit.MILLISECONDS));
 	}
 }
