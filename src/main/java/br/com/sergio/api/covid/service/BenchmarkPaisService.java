92631ee9248764038b6f281f78e078f6dbd6be0b
Sérgio Barbosa <sergio.natan@oobj.com.br>
2024-03-24 21:16:52 -0300

    Adaptando métodos para retorno de DTOs

--- src/main/java/br/com/sergio/api/covid/service/BenchmarkPaisService.java
+++ src/main/java/br/com/sergio/api/covid/service/BenchmarkPaisService.java
@@ -4,32 +4,28 @@ import br.com.sergio.api.covid.external.ExternalPacoteCasosEMortes;
 import br.com.sergio.api.covid.external.service.ExternalDadosPaisesServices;
 import br.com.sergio.api.covid.model.BenchmarkPais;
 import br.com.sergio.api.covid.processador.ProcessadorBenchmarkPais;
-import br.com.sergio.api.covid.utils.json.ConversorDados;
+import br.com.sergio.api.covid.rest.dto.BenchmarkPaisDTO;
+import br.com.sergio.api.covid.rest.dto.factory.BenchmarkDTOFactory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 @Service
 public class BenchmarkPaisService {
 	
 	private final ProcessadorBenchmarkPais processadorBenchmarkPais;
 	private final ExternalDadosPaisesServices externalPaisesServices;
-	private final ConversorDados conversorDados;
+	private final BenchmarkDTOFactory benchmarkDTOFactory;
 	private static final Logger LOG = LogManager.getLogger(BenchmarkPaisService.class);
 	
 	public BenchmarkPaisService(ProcessadorBenchmarkPais processadorBenchmarkPais,
-								ExternalDadosPaisesServices externalPaisesServices, ConversorDados conversorDados) {
+								ExternalDadosPaisesServices externalPaisesServices, BenchmarkDTOFactory benchmarkDTOFactory) {
 		this.processadorBenchmarkPais = processadorBenchmarkPais;
 		this.externalPaisesServices = externalPaisesServices;
-		this.conversorDados = conversorDados;
-	}
-	
-	
-	public String obtemJsonBenchmarkPais(String pais, String stringDataInicial, String stringDataFinal) {
-		BenchmarkPais benchmarkPais = obtemBenchmarkPais(pais, stringDataInicial, stringDataFinal);
-		return conversorDados.converterDadosParaJson(benchmarkPais, BenchmarkPais.class);
+		this.benchmarkDTOFactory = benchmarkDTOFactory;
 	}
 	
 	public BenchmarkPais obtemBenchmarkPais(String pais, String stringDataInicial, String stringDataFinal) {
@@ -41,6 +37,9 @@ public class BenchmarkPaisService {
 		return processadorBenchmarkPais.processar(externalPacoteCasosEMortes);
 	}
 	
+	public BenchmarkPaisDTO obtemBenchmarkPaisDTO(String pais, String stringDataInicial, String stringDataFinal){
+		return benchmarkDTOFactory.geraDTOBenchmarkPais(obtemBenchmarkPais( pais,  stringDataInicial,  stringDataFinal));
+	}
 	
 	private LocalDate converteStringParaData(String stringData) {
 		return LocalDate.parse(stringData, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
