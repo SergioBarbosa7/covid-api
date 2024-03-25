694e161af457d162484405e52baf07b723fc4284
Sérgio Barbosa <sergio.natan@oobj.com.br>
2024-03-24 22:05:06 -0300

    Adicionando validação de existência do país para o preenchimento e requisições da api

--- src/main/java/br/com/sergio/api/covid/external/service/ExternalDadosPaisesServices.java
+++ src/main/java/br/com/sergio/api/covid/external/service/ExternalDadosPaisesServices.java
@@ -8,6 +8,8 @@ import br.com.sergio.api.covid.external.ExternalEnvelopeDadosPorPais;
 import br.com.sergio.api.covid.external.ExternalListaEnvelopesDadosPorPais;
 import br.com.sergio.api.covid.external.ExternalPacoteCasosEMortes;
 import br.com.sergio.api.covid.external.handler.ExternalDadosPaisesHandler;
+import br.com.sergio.api.covid.model.Pais;
+import br.com.sergio.api.covid.service.PaisService;
 import br.com.sergio.api.covid.utils.ConstrutorDeURL;
 import br.com.sergio.api.covid.utils.json.ConversorDados;
 import org.springframework.stereotype.Service;
@@ -17,19 +19,22 @@ public class ExternalDadosPaisesServices {
 	
 	private final ConversorDados conversorDados;
 	private final ConsumoApi consumoApi;
+	private final PaisService paisService;
 	private final ConstrutorDeURL construtorDeURL;
 	private final ExternalDadosPaisesHandler paisesHandler;
 	
 	
-	public ExternalDadosPaisesServices(ConsumoApi consumoApi, ExternalDadosPaisesHandler paisesHandler, ConstrutorDeURL construtorDeURL, ConversorDados conversorDados) {
+	public ExternalDadosPaisesServices(ConsumoApi consumoApi, PaisService paisService, ExternalDadosPaisesHandler paisesHandler,
+									   ConstrutorDeURL construtorDeURL, ConversorDados conversorDados) {
 		this.consumoApi = consumoApi;
+		this.paisService = paisService;
 		this.paisesHandler = paisesHandler;
 		this.construtorDeURL = construtorDeURL;
 		this.conversorDados = conversorDados;
 	}
 	
 	
-	public String obtemJsonDoEnvelope(String pais, String tipo)  {
+	public String obtemJsonDoEnvelope(String pais, String tipo) {
 		ExternalEnvelopeDadosPorPais envelope = obtemEnvelopeDadosPais(pais, tipo);
 		return conversorDados.converterDadosParaJson(envelope, ExternalEnvelopeDadosPorPais.class);
 	}
@@ -42,14 +47,15 @@ public class ExternalDadosPaisesServices {
 		return paisesHandler.trataPaisesComRegioes(listaEnvelopesDadosPorPais);
 	}
 	
-	public ExternalPacoteCasosEMortes obtemPacoteCasosEMortes(String pais)  {
-		ExternalEnvelopeDadosPorPais envelopeCasos = obtemEnvelopeDadosPais(pais, CASOS);
-		ExternalEnvelopeDadosPorPais envelopeMortes = obtemEnvelopeDadosPais(pais, MORTES);
+	public ExternalPacoteCasosEMortes obtemPacoteCasosEMortes(String nomePais){
+		Pais pais = paisService.obtemPaisPeloNome(nomePais);
+		ExternalEnvelopeDadosPorPais envelopeCasos = obtemEnvelopeDadosPais(pais.getNomeIngles(), CASOS);
+		ExternalEnvelopeDadosPorPais envelopeMortes = obtemEnvelopeDadosPais(pais.getNomeIngles(), MORTES);
 		
 		ExternalPacoteCasosEMortes pacote = new ExternalPacoteCasosEMortes();
 		pacote.setDadosCasos(envelopeCasos.getMapDataParaDados());
 		pacote.setDadosMortes(envelopeMortes.getMapDataParaDados());
-		pacote.setPais(pais);
+		pacote.setPais(pais.getNomePortugues());
 		return pacote;
 	}
 }
